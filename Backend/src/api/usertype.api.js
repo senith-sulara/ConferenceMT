076f06d402b5db923340203b65275ded49bb2495
senith-sulara <senithsula00@gmail.com>
2021-06-29 19:23:43 +0530

    init

--- Backend/src/api/usertype.api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/usertype.controller');
-
-module.exports = function () {
-  router.post('/Utype', controller.createType);
-  router.get('/', controller.getAllTypes);
-  return router;
-}
--- Backend/src/models/usertype.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-let Usertype = new Schema({
-  name: {type: String,required: true, trim: true},
-  users: [{ type: mongoose.Schema.Types.ObjectId, required: false, ref: 'users' }],
-});
-
-module.exports = mongoose.model("usertype", Usertype);
