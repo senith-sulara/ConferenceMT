8cf8d6bc7b158df523fbb9cbd09840a450ee26d0
senith-sulara <65754189+senith-sulara@users.noreply.github.com>
2021-06-30 17:00:10 +0530

    Delete Backend directory

--- Backend/config/db.config.js
+++
@@ -1,6 +0,0 @@
-//connect mongodb database
-module.exports = {
-    url:
-      "mongodb+srv://conferenceDb:conDB123@conference.yn2ko.mongodb.net/ConferenceDB?retryWrites=true&w=majority",
-  };
-  
